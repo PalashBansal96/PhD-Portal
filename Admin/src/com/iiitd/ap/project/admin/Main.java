7db1d5a42b4fb7bdb75daec95361421a28a887c9
Palash Bansal <PalashBansal96@gmail.com>
2015-09-13 20:00:41 +0530

    Basic Project Structure

---
+++ Admin/src/com/iiitd/ap/project/admin/Main.java
@@ -0,0 +1,22 @@
+/**
+ * @author Palash Bansal
+ * @date 9/13/2015
+
+ */
+
+package com.iiitd.ap.project.admin;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+
+	}
+}
---
+++ Applicant/src/com/iiitd/ap/project/Main.java
@@ -0,0 +1,21 @@
+/**
+ * @author Palash
+ * @date 9/13/2015
+ */
+
+package com.iiitd.ap.project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+}
